45635a12ac5822ba0b264ea3e36ae94466a3305c
Joe <V.josephraj26@gmail.com>
2024-11-14 11:41:01 -0500

    Added code to replace placeholders in CV_template.docx

--- README.md
+++ README.md
@@ -5,10 +5,16 @@
 1. [Project Overview](#project-overview)
 2. [Features](#key-features)
 3. [Technologies Used](#technologies-used)
-4. [Design Schema](#design-schema)
-5. [Installation Guide](#installation-guide)
+4. [Installation Guide](#installation-guide)
 
 
 ## Project Overview
 
 **JobVaultGenerator** is a application built in Python using Selenium
+
+## Technologies Used
+1. Python
+2. Selenium
+3. python 'docx' module
+
+## Installation Guide
--- main.py
+++ main.py
@@ -2,29 +2,40 @@ import click
 import os
 import shutil
 import time
+from docx import Document
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 
+def make_cv(data):
+    print(data['cv_info'].items())
+    doc = Document('CV_template.docx')
+    for paragraph in doc.paragraphs:
+        for placeholder, value in data['cv_info'].items():
+            for run in paragraph.runs:
+                if placeholder in run.text:
+                    run.text = run.text.replace(placeholder, value)
+        
+    doc.save('CV.docx')
+
 def copy_files(path, resume_num, company, job_folder):
     # Copy resume.docx to Job folder
     if resume_num == 1:
-        shutil.copy(path+'resume_store/devops.docx', path+company+'/'+job_folder+'/'+'Joseph_2024.docx')
+        shutil.copy(path+'resume_store/devops.docx', path+company+'/'+job_folder+'/Joseph_2024.docx')
     elif resume_num == 2:
-        shutil.copy(path+'resume_store/data.docx', path+company+'/'+job_folder+'/'+'Joseph_2024.docx')
+        shutil.copy(path+'resume_store/data.docx', path+company+'/'+job_folder+'/Joseph_2024.docx')
     elif resume_num == 3:
-        shutil.copy(path+'resume_store/pythondev.docx', path+company+'/'+job_folder+'/'+'Joseph_2024.docx')
+        shutil.copy(path+'resume_store/pythondev.docx', path+company+'/'+job_folder+'/Joseph_2024.docx')
     elif resume_num == 4:
-        shutil.copy(path+'resume_store/javadev.docx', path+company+'/'+job_folder+'/'+'Joseph_2024.docx')
+        shutil.copy(path+'resume_store/javadev.docx', path+company+'/'+job_folder+'/Joseph_2024.docx')
     elif resume_num == 5:
-        shutil.copy(path+'resume_store/cloud.docx', path+company+'/'+job_folder+'/'+'Joseph_2024.docx')
+        shutil.copy(path+'resume_store/cloud.docx', path+company+'/'+job_folder+'/Joseph_2024.docx')
     
     # Copy cv.docx to Job folder
-    shutil.copy(path+'Joseph_24_CL.docx', path+company+'/'+job_folder)
+    shutil.move(path+'CV.docx', path+company+'/'+job_folder+'/CV.docx')
 
     # Copy jd.pdf to Job folder
-    shutil.copy(path+'JD.png', path+company+'/'+job_folder)
-
+    shutil.move(path+'JD.png', path+company+'/'+job_folder)
 
 def make_jd_file(jobLink):
     options = Options()
@@ -40,43 +51,46 @@ def make_jd_file(jobLink):
     driver.get_full_page_screenshot_as_file('JD.png')
     time.sleep(2)    
     
-    # Get the entire page content and save it to a text file
+    # Get the entire page content 
     page_text = driver.find_element("tag name", "body").text
+
+    # save it to a text file
+    """
     with open('JD.txt', 'w', encoding='utf-8') as f:
         f.write(page_text)
-
-    driver.quit()
-
-
-"""
-def get_job_desc(jobLink):
-    job_description = ''
+    """
     
-    # Make a JD.pdf and place in root location
-    make_jd_file(jobLink)
-
-    return job_description
-"""
-
+    driver.quit()
+    return(page_text)
 
 def folder_present(path, company_folder):
     return os.path.exists(path+company_folder)
-
 
 @click.command()
 @click.option('--job_link', required=True, help='Enter the Job Link')
 def main(job_link):
     path = '/Users/joseph/Documents/PythonProjects/JobVault/'
     data = {}
+
     data["company"] = input('Enter Company Name: \n')
     data["job_link"] = job_link
     data["job_id"] = input('Enter Job Id: \n')
     data["position"] = input('Enter Position: \n')
     data["resume_num"] = int(input('Which Resume?(enter number) \n1. DevOps \n2. Data \n3. PythonDev \n4. JavaDev \n5. Cloud\n'))
-
+    data["cv_info"] = {
+        '{NAME}': 'Josephraj Velpula',
+        '{LOCATION}': 'Okemos, MI',
+        '{MOBILE}': '(404)-916-1803',
+        '{EMAILID}': 'V.josephraj26@gmail.com',
+        '{DATE}': '14 November, 2024',
+        '{COMPANY}': str(data['company'])
+    }
 
     # Get Job Details from the website
     data["job_desc"] = make_jd_file(data['job_link'])
+
+    # Add data to CV_template  
+    make_cv(data)
 
     # Check Company name folder
     if (not folder_present(path, data["company"])):
